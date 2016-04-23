ae895e1cd6f62d836fb231abf6d5b6aa5d7355b8
James Campbell <james@supmenow.com>
2016-04-23 19:26:22 +0100

    Preparing for time indicators.

--- Silkscreen/Layout/TimelineCollectionViewLayout.swift
+++ Silkscreen/Layout/TimelineCollectionViewLayout.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class TimelineCollectionViewLayout: UICollectionViewLayout {
+    
+    override init() {
+        super.init()
+        
+        // Do time indicators at the top.
+       // registerClass(<#T##viewClass: AnyClass?##AnyClass?#>, forDecorationViewOfKind: <#T##String#>)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
  
     override func collectionViewContentSize() -> CGSize {
         return CGSize(width: CGFloat.max, height: 10)

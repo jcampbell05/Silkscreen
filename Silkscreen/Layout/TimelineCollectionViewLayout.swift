7c38d47c779eebd479cc0e7cc6869c0353a6160e
James Campbell <james@supmenow.com>
2016-04-23 19:17:03 +0100

    Layout implemented.

---
+++ Silkscreen/Layout/TimelineCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  TimelineCollectionViewLayout.swift
+//  Silkscreen
+//
+//  Created by James Campbell on 4/23/16.
+//  Copyright © 2016 SK. All rights reserved.
+//
+
+import UIKit
+
+class TimelineCollectionViewLayout: UICollectionViewLayout {
+    
+}
--- Silkscreen/ViewControllers/TimelineViewController.swift
+++ Silkscreen/ViewControllers/TimelineViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class TimelineViewController: UIViewController {
+class TimelineViewController: UICollectionViewController {
+    
+    let layout = TimelineCollectionViewLayout()
+    
+    init() {
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.grayColor()
     }
 }

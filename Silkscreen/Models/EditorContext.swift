b31a04de0b12532b914a03c650915cf248446a43
James Campbell <james@supmenow.com>
2016-04-24 22:25:04 +0100

    Preparing context.

---
+++ Silkscreen/Models/EditorContext.swift
@@ -0,0 +1,13 @@
+//
+//  EditorContext.swift
+//  Silkscreen
+//
+//  Created by James Campbell on 4/24/16.
+//  Copyright © 2016 SK. All rights reserved.
+//
+
+import AVKit
+
+class EditorContext {
+    
+}
